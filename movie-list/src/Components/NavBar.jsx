532a298a7809a2415d772d6d1cc03c8f959b934b
Winner <vnwimo13@gmail.com>
2023-09-11 11:42:12 +0100

    starting

---
+++ movie-list/src/Components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <>
+      <nav>
+        <div>
+          <img src="../" alt="" />
+        </div>
+      </nav>
+    </>
+  );
+}
